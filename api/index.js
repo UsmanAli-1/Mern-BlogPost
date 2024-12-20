b4ee16449d4a9848ac6104876486127cd456bfae
usman ali <uali75616@gmail.com>
2024-12-20 23:32:18 +0500

    Fix login route and improve error handling

--- api/index.js
+++ api/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const { default: mongoose, connect } = require('mongoose');
 const User = require('./models/user');
+const Post = require('./models/post');
 const bcrypt = require('bcrypt'); //used to ecrypt data
 const app = express();
-const jwt = require('jsonwebtoken'); 
+const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' });
+const fs = require('fs');
 
-app.use(cors({credentials:true,origin:"http://localhost:3000"}));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -20,8 +24,8 @@ app.post('/register', async (req, res) => {
         try {
                 const userDoc = await User.create({
                         username,
-                        password:bcrypt.hashSync(password, salt), //used to ecrypt data
-                }); 
+                        password: bcrypt.hashSync(password, salt), //used to ecrypt data
+                });
                 res.json(userDoc);
         }
         catch (e) {
@@ -30,21 +34,57 @@ app.post('/register', async (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-        const { username, password } = req.body;        
-        const userDoc = await User.findOne({username});
+        const { username, password } = req.body;
+        const userDoc = await User.findOne({ username });
         const passOk = bcrypt.compareSync(password, userDoc.password);
-        
-        if(passOk){
-                jwt.sign({username,id:userDoc._id} , secret , {} , (err , token)=>{
-                        if(err) throw err;
-                        res.cookie('token' , token).json('ok');
+
+        if (passOk) {
+                jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+                        if (err) throw err;
+                        res.cookie('token', token).json({
+                                id: userDoc._id,
+                                username,
+                        });
                 });
-        }else{
+        } else {
                 res.status(400).json('Wrong credentials');
         }
 });
 
-app.get('/profile' , (req,res)=>{
-        res.json(req.cookies)
+app.get('/profile', (req, res) => {
+        const { token } = req.cookies;
+        jwt.verify(token, secret, {}, (err, info) => {
+                if (err) throw err;
+                res.json(info);
+        })
 });
+
+// to logout the user 
+app.post('/logout', (req, res) => {
+        res.cookie('token', '').json('ok')
+})
+
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+
+
+        const { originalname, path } = req.file;
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1];
+        const newPath = path + '.' + ext;
+        fs.renameSync(path, newPath);
+
+        const { title, summary, content } = req.body;
+
+        const postDoc = await Post.create({
+                title,
+                summary,
+                content,
+                cover:newPath,
+        });
+        res.json(postDoc);
+
+
+});
+
 app.listen(4000, () => console.log("Server is running on port 4000"));
+
